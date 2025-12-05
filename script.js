dfe560d15f5973783af9638c4d4930e557eb05f9
Minnie The Dev <minniethedev@gmail.com>
2025-12-05 14:35:09 +1100

    Write logic to get computer hand choice

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 100);
+    
+    let computerChoice;
+
+    if (randomNumber <= 33) {
+        computerChoice = "rock";
+    }
+    else if (randomNumber > 33 && randomNumber <= 66) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors"
+    }
+
+    return computerChoice
+}
+
+getComputerChoice();
